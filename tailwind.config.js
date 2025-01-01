c0df7f3605c9e199627f32a42e3129bb090348f7
Alan-Cheng <jhcheng.alan@gmail.com>
2025-01-01 14:40:43 +0800

    style: 更新參考模板

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
       colors: {
-        orange: {
-          100: '#FFEDD5', // 添加 Tailwind 預設的淺橘色
-          200: '#FED7AA',
-          500: '#F97316',
-          900: '#7C2D12',
+        primary: {
+          light: '#F4EFE6',
+          DEFAULT: '#A18249',
+          dark: '#1C160C',
         },
-        baseColor: '#F4EFE6',
-        noteColor: '#A18249'
+        success: '#019863',
+        white: '#FFFFFF',
       },
     },
   },
+  plugins: [],
 };
+
 
