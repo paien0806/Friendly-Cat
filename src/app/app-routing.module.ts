92d54320e96a2414aabc9879d38c438daa35c10d
Alan-Cheng <jhcheng.alan@gmail.com>
2025-01-05 00:52:46 +0800

    fix: 路徑錯誤

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NewSearchComponent } from './search-food/new-search/new-search.component';
-import { LoginPageComponent } from './components/login-page/login-page.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/search', pathMatch: 'full' }, // 空路徑時重定向到 /search
-  { path: 'search', component: NewSearchComponent },      // 搜索頁面
-  { path: 'login', component: LoginPageComponent },       // 登入頁面
-  { path: '**', redirectTo: '/search' },                  // 其他所有路徑重定向到 /search
+  { path: '**', component: NewSearchComponent },  // 所有其他路径都重定向到 /search
 ];
 
 @NgModule({
