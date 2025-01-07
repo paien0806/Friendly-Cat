53f7a0163eb2c38d57fbc5c28f1b83d4d92a2ad5
Alan-Cheng <jhcheng.alan@gmail.com>
2025-01-07 13:35:06 +0800

    new: 新增iOS APP Project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.alancheng.FriendlyCat',
+  appName: 'Friendly-Cat',
+  webDir: 'dist'
+};
+
+export default config;
