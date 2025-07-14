30669e9e34054cb7f2dde04ae8e7cbba3b94fe6f
paien0806 <chengpaien@gmail.com>
2025-07-14 16:45:47 +0800

    Create friendlycat.py

---
+++ friendlycat.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def get_items():
+    data = {
+        "items": [
+            {"name": "即期雞蛋", "price": 25, "store": "全家台北店"},
+            {"name": "優格", "price": 10, "store": "7-11 忠孝店"},
+        ]
+    }
+    return data
