b71c20fccb7f0374e83675c858b91364ea4455d5
paien0806 <chengpaien@gmail.com>
2025-07-14 16:57:16 +0800

    Update friendlycat.py

--- friendlycat.py
+++ friendlycat.py
@@ -1,7 +1,9 @@
 def get_items():
-    return [
+    return {
+        "items": [
         {"name": "7-11 焗烤雞腿飯", "price": 79},
         {"name": "全家 照燒雞三明治", "price": 49},
         {"name": "全聯 草莓牛奶", "price": 32},
         {"name": "OK 超商 鮪魚飯糰", "price": 35}
-    ]
+        ]
+    }
